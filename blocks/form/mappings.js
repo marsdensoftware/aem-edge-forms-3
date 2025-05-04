ee6710c13d5d7e249d5af254fc936b7cddbad7b8
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-05 11:11:05 +1200

    Adding range custom component

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['form-hero'];
+let customComponents = ['range'];
 const OOTBComponentDecorators = ['file-input', 'wizard', 'modal', 'tnc', 'toggleable-link', 'rating', 'datetime', 'list', 'location', 'accordion', 'password'];
 
 export function setCustomComponents(components) {
