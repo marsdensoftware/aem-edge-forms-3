6d8ef6d4db29aa89f822ff5186c917ef895aa1b6
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-05 15:31:27 +1200

    updated js

--- blocks/form/components/formwelcome/formwelcome.js
+++ blocks/form/components/formwelcome/formwelcome.js
@@ -2,7 +2,7 @@ export default function decorate(panel) {
   panel.classList.add('panel-formwelcome');
   
   // select all items
-  const items = panel.querySelectorAll('>fieldset>fieldset');
+  const items = panel.querySelectorAll(':scope>fieldset>fieldset');
   items.forEach((e)=>{
       e.classList.add('panel-formwelcome-item');
   });
