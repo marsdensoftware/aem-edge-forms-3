b16f206a77e7205d160248260350966a77265440
Seno <aseno001cladmin@msd.govt.nz>
2025-05-06 20:54:45 +1200

    chore(dep): minify css output

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -4,32 +4,34 @@ const gulpSass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const pxtorem = require('postcss-pxtorem')
+const postcssMinify = require('@csstools/postcss-minify')
 
 const sass = gulpSass(dartSass)
 const plugin = [
-    autoprefixer({}),
-    pxtorem({
-        rootValue: 16,
-        unitPrecision: 4,
-        propList: ['*', '!border', '!border-*'],
-        selectorBlackList: ['letter-spacing', 'border'],
-        replace: true,
-        mediaQuery: false,
-        minPixelValue: 0,
-        exclude: /node_modules/i,
-    }),
+  autoprefixer({}),
+  pxtorem({
+    rootValue: 16,
+    unitPrecision: 4,
+    propList: ['*', '!border', '!border-*'],
+    selectorBlackList: ['letter-spacing', 'border'],
+    replace: true,
+    mediaQuery: false,
+    minPixelValue: 0,
+    exclude: /node_modules/i,
+  }),
+  postcssMinify(),
 ]
 
 const styleFolders = ['blocks/**/*.scss', 'styles/**/*.scss']
 
 const style = () =>
-    src(styleFolders, { base: './' })
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(plugin))
-        .pipe(dest('./'))
+  src(styleFolders, { base: './' })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugin))
+    .pipe(dest('./'))
 
 const watching = () => {
-    watch(styleFolders, series(style))
+  watch(styleFolders, series(style))
 }
 
 exports.default = watching
