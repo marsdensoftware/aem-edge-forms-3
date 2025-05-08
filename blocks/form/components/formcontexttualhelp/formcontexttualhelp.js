b8ad1917c4d1950d74cfede708cae783578bb264
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-08 18:06:55 +1200

    feat(contextualhelp)

--- blocks/form/components/formcontexttualhelp/formcontexttualhelp.js
+++ blocks/form/components/formcontexttualhelp/formcontexttualhelp.js
@@ -1,6 +1,6 @@
 export default function decorate(panelEl, model) {
-    const properties = { model };
-    
+    const { properties } = model;
+
     panelEl.classList.add('panel-formcontexttualhelp')
 
     if (properties.link && properties.linkText) {
@@ -8,14 +8,33 @@ export default function decorate(panelEl, model) {
         const footerEl = document.createElement("div");
 
         // Optionally, add content or attributes
-        footerEl.className = 'panel-formcontextualhelp__footer';
+        footerEl.className = 'panel-formcontextualhelp__footer field-wrapper';
 
         // Create the anchor element
         const linkEl = document.createElement("a");
 
         // Set its text and href
-        linkEl.textContent = properties.linkText;
         linkEl.href = properties.link;
+        linkEl.classList.add('panel-formcontextualhelp__footer__link');
+
+        if (properties.linkOpenInNewTab) {
+            linkEl.target = '_blank';
+
+            // add icon for external link
+            // Create the span element for the icon
+            const iconEl = document.createElement('span');
+            // Add a class for styling
+            iconEl.classList.add('panel-formcontextualhelp__footer__link-icon');
+
+            // Create the span element for the text
+            const textEl = document.createElement('span');
+            textEl.textContent = properties.linkText;
+            textEl.classList.add('panel-formcontextualhelp__footer__link-text');
+
+            // Append the span elements to the a element
+            linkEl.appendChild(textEl);
+            linkEl.appendChild(iconEl);
+        }
 
         // Optionally, open in new tab
         // linkEl.target = "_blank";
