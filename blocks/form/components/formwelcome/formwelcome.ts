827dfbd6c3fed0de592e82f89354287d1a8b489b
Seno <aseno001cladmin@msd.govt.nz>
2025-05-08 19:29:43 +1200

    feat(form): job seeker profile intro welcome initial

--- blocks/form/components/formwelcome/formwelcome.ts
+++ blocks/form/components/formwelcome/formwelcome.ts
@@ -1,10 +1,15 @@
 export default function decorate(panel: Element) {
   panel.classList.add('panel-formwelcome')
 
-  // select all items
+  const div = panel.querySelector('div')
+  div!.className = 'panel-formwelcome__header'
+
   const items = panel.querySelectorAll(':scope>fieldset>fieldset')
   items.forEach((e) => {
-    e.classList.add('panel-formwelcome-item')
+    e.classList.add('panel-formwelcome__item')
+    const divs = e.querySelectorAll('div')
+    divs[0].className = 'panel-formwelcome__item-image'
+    divs[1].className = 'panel-formwelcome__item-content'
   })
   return panel
 }
