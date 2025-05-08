33df4c5972b8b235a6f90b26feca9ebe4fce5d9f
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-08 18:16:58 +1200

    feat(contexttualhelp) link in new tab

--- blocks/form/components/formcontexttualhelp/formcontexttualhelp.js
+++ blocks/form/components/formcontexttualhelp/formcontexttualhelp.js
@@ -17,27 +17,25 @@ export default function decorate(panelEl, model) {
         linkEl.href = properties.link;
         linkEl.classList.add('panel-formcontextualhelp__footer__link');
 
+        // Create the span element for the text
+        const textEl = document.createElement('span');
+        textEl.textContent = properties.linkText;
+        textEl.classList.add('panel-formcontextualhelp__footer__link-text');
+
+        // Append the span elements to the a element
+        linkEl.appendChild(textEl);
+
         if (properties.linkOpenInNewTab) {
             linkEl.target = '_blank';
 
-            // add icon for external link
+            // Add icon for external link
             // Create the span element for the icon
             const iconEl = document.createElement('span');
             // Add a class for styling
             iconEl.classList.add('panel-formcontextualhelp__footer__link-icon');
 
-            // Create the span element for the text
-            const textEl = document.createElement('span');
-            textEl.textContent = properties.linkText;
-            textEl.classList.add('panel-formcontextualhelp__footer__link-text');
-
-            // Append the span elements to the a element
-            linkEl.appendChild(textEl);
             linkEl.appendChild(iconEl);
         }
-
-        // Optionally, open in new tab
-        // linkEl.target = "_blank";
 
         // Append it to the existing element
         footerEl.appendChild(linkEl);
