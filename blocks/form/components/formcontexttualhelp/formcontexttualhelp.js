27fcb1c2c71ff77f0d05fe3edc0d02ecbd4ef41d
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-08 17:50:33 +1200

    feat(form) contextualhelp

--- blocks/form/components/formcontexttualhelp/formcontexttualhelp.js
+++ blocks/form/components/formcontexttualhelp/formcontexttualhelp.js
@@ -1,4 +1,31 @@
-export default function decorate(panel) {
-  panel.classList.add('panel-formcontexttualhelp')
-  return panel
+export default function decorate(panelEl, model) {
+    const properties = { model };
+    
+    panelEl.classList.add('panel-formcontexttualhelp')
+
+    if (properties.link && properties.linkText) {
+        // Create the footer div
+        const footerEl = document.createElement("div");
+
+        // Optionally, add content or attributes
+        footerEl.className = 'panel-formcontextualhelp__footer';
+
+        // Create the anchor element
+        const linkEl = document.createElement("a");
+
+        // Set its text and href
+        linkEl.textContent = properties.linkText;
+        linkEl.href = properties.link;
+
+        // Optionally, open in new tab
+        // linkEl.target = "_blank";
+
+        // Append it to the existing element
+        footerEl.appendChild(linkEl);
+
+        // Append the new div to the existing element
+        panelEl.appendChild(footerEl);
+    }
+
+    return panelEl;
 }
