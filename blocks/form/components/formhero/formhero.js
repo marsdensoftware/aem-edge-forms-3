8ad126ff321b2d31831883eac274709e50f266dc
Seno <aseno001cladmin@msd.govt.nz>
2025-05-08 17:58:11 +1200

    feat(form): job seeker profile intro hero

--- blocks/form/components/formhero/formhero.js
+++ blocks/form/components/formhero/formhero.js
@@ -1,4 +1,7 @@
 export default function decorate(panel) {
-  panel.classList.add('panel-heroform')
-  return panel
+    panel.classList.add('panel-heroform');
+    const divs = panel.querySelectorAll('div');
+    divs[0].className = 'panel-heroform__picture';
+    divs[1].className = 'panel-heroform__content';
+    return panel;
 }
--- blocks/form/components/formwelcome/formwelcome.js
+++ blocks/form/components/formwelcome/formwelcome.js
@@ -1,10 +1,9 @@
 export default function decorate(panel) {
-  panel.classList.add('panel-formwelcome')
-
-  // select all items
-  const items = panel.querySelectorAll(':scope>fieldset>fieldset')
-  items.forEach((e) => {
-    e.classList.add('panel-formwelcome-item')
-  })
-  return panel
+    panel.classList.add('panel-formwelcome');
+    // select all items
+    const items = panel.querySelectorAll(':scope>fieldset>fieldset');
+    items.forEach((e) => {
+        e.classList.add('panel-formwelcome-item');
+    });
+    return panel;
 }
