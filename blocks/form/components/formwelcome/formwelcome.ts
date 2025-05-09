1234d762b376013476e70ac02c36322bfd958c66
Seno <aseno001cladmin@msd.govt.nz>
2025-05-09 14:18:22 +1200

    feat(form): content grid, wizard spacing

--- blocks/form/components/formwelcome/formwelcome.ts
+++ blocks/form/components/formwelcome/formwelcome.ts
@@ -1,15 +1,26 @@
 export default function decorate(panel: Element) {
   panel.classList.add('panel-formwelcome')
 
+  const container = document.createElement('div')
+  container.className = 'panel-formwelcome__content-container'
+  const directFieldsetChild = panel.querySelector(':scope > fieldset')
+  directFieldsetChild?.classList.add('panel-formwelcome__container')
+
+  directFieldsetChild?.append(container)
+
   const div = panel.querySelector('div')
   div!.classList.add('panel-formwelcome__header')
 
-  const items = panel.querySelectorAll(':scope>fieldset>fieldset')
-  items.forEach((e) => {
-    e.classList.add('panel-formwelcome__item')
-    const divs = e.querySelectorAll('div')
+  const fieldsets = panel.querySelectorAll(':scope>fieldset>fieldset')
+
+  fieldsets.forEach((el) => {
+    el.classList.add('panel-formwelcome__item')
+    container.append(el)
+
+    const divs = el.querySelectorAll('div')
     divs[0].classList.add('panel-formwelcome__item-content')
     divs[1].classList.add('panel-formwelcome__item-image')
   })
+
   return panel
 }
