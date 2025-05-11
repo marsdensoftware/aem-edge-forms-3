c0c297e334b4c530b17d90b57230d6bf02d844c0
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-12 10:57:51 +1200

    feat(form) customizations rte tinymce hook

---
+++ scripts/editor-support-rte-customization.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable no-cond-assign */
+/* eslint-disable import/prefer-default-export */
+
+// group editable texts in single wrappers if applicable.
+// this script should execute after script.js but before the the universal editor cors script
+// and any block being loaded
+
+console.log('Editor Customizations');
