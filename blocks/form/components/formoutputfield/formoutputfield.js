3c17ecaf299bdccb1fe9fd2ca3a674bc3ea3e919
Nono Junang <nono.junang@insideaem.com>
2025-05-13 17:28:32 +1200

    Update formoutputfield.js

--- blocks/form/components/formoutputfield/formoutputfield.js
+++ blocks/form/components/formoutputfield/formoutputfield.js
@@ -1,6 +1,30 @@
 
+function waitForWindowObject(key) {
+    return new Promise((resolve) => {
+        const check = () => {
+            if (window[key]) {
+                resolve(window[key]);
+            } else {
+                setTimeout(check, 100); // check again in 100ms
+            }
+        };
+
+        check();
+    });
+}
+
 export default function decorate(fieldDiv, fieldJson) {
-  // get the input element from the fieldDiv
-  
-  return fieldDiv;
+    // get the input element from the fieldDiv
+    waitForWindowObject('myForm').then((form) => {
+        console.log('Form is available:', form);
+        const field = form.getElement(fieldJson.id);
+        if (field) {
+            field.subscribe((e) => {
+                // Subscribe to field change event
+
+            }, 'fieldChanged');
+        }
+    });
+
+    return fieldDiv;
 }
