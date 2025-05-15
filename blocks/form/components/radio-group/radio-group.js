1b0213e54b920ecf59adb6de3281888fc9e42650
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-16 11:21:45 +1200

    feat(form) moved js code for radio with icon into custom component

--- blocks/form/components/radio-group/radio-group.js
+++ blocks/form/components/radio-group/radio-group.js
@@ -3,16 +3,19 @@ export default function decorate(element, field, container) {
 
     if (withIcon) {
         element.classList.add('radio-group-wrapper--with-icon');
-    }
-    const radios = element.querySelectorAll('input[type="radio"]');
-    
-    enumIconNames?.forEach((iconName, index) => {
-        const radio = radios[index];
+        const radios = element.querySelectorAll('input[type="radio"]');
 
-        if (radio && withIcon && iconName) {
-            radio.classList.add(`icon-${iconName}`);
+        if (radios) {
+            enumIconNames?.forEach((iconName, index) => {
+                const radio = radios[index];
+
+                if (radio && withIcon && iconName) {
+                    radio.classList.add(`icon-${iconName}`);
+                }
+            });
         }
-    });
+
+    }
 
     return element;
 }
