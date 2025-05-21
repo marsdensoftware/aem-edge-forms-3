2cec052b58e2f3612b328c9f0367de1da2c73b76
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-22 09:23:20 +1200

    feat(form) form welcome fix

--- blocks/form/components/formwelcome/formwelcome.js
+++ blocks/form/components/formwelcome/formwelcome.js
@@ -1,16 +1,14 @@
 export default function decorate(panel) {
     panel.classList.add('panel-formwelcome');
-    const container = document.createElement('div');
-    container.className = 'panel-formwelcome__content-container';
-    const directFieldsetChild = panel.querySelector(':scope > fieldset');
-    directFieldsetChild === null || directFieldsetChild === void 0 ? void 0 : directFieldsetChild.classList.add('panel-formwelcome__container');
-    directFieldsetChild === null || directFieldsetChild === void 0 ? void 0 : directFieldsetChild.append(container);
-    const div = panel.querySelector('div');
-    div.classList.add('panel-formwelcome__header');
-    const fieldsets = panel.querySelectorAll(':scope>fieldset>fieldset');
-    fieldsets.forEach((el) => {
+    const children = panel.querySelectorAll(':scope > fieldset');
+    // container for text
+    children[0].classList.add('panel-formwelcome__header');
+    // container for items
+    const container = children[1];
+    children[1].classList.add('panel-formwelcome__content-container');
+    const items = container.querySelectorAll(':scope > fieldset');
+    items.forEach((el) => {
         el.classList.add('panel-formwelcome__item');
-        container.append(el);
         const divs = el.querySelectorAll('div');
         divs[0].classList.add('panel-formwelcome__item-content');
         divs[1].classList.add('panel-formwelcome__item-image');
