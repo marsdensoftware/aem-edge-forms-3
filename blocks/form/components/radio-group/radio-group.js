1045ebadba2f550401305b502533c881555ed18c
Seno <aseno001cladmin@msd.govt.nz>
2025-05-22 14:26:13 +1200

    feat(form): radio button with icon

--- blocks/form/components/radio-group/radio-group.js
+++ blocks/form/components/radio-group/radio-group.js
@@ -1,21 +1,29 @@
-export default function decorate(element, field, container) {
+export default function decorate(element, field) {
     const { withIcon, enumIconNames } = field.properties;
-
     if (withIcon) {
-        element.classList.add('radio-group-wrapper--with-icon');
-        const radios = element.querySelectorAll('input[type="radio"]');
-
-        if (radios) {
-            enumIconNames?.forEach((iconName, index) => {
-                const radio = radios[index];
-
-                if (radio && withIcon && iconName) {
-                    radio.classList.add(`icon-${iconName}`);
-                }
+        element.classList.add('radio-group--with-icon');
+        const divs = element.querySelectorAll('div');
+        if (divs.length) {
+            enumIconNames.forEach((iconName, index) => {
+                const div = divs[index];
+                // LABEL
+                const label = div.querySelector('label');
+                label === null || label === void 0 ? void 0 : label.classList.add('radio-group--with-icon__label');
+                // ICON
+                const icon = document.createElement('span');
+                icon.className = `.radio-group--with-icon .radio-group--with-icon--${iconName}`;
+                icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="45" height="44" viewBox="0 0 45 44" fill="none">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M26.2181 8.55664L27.5978 9.93623L22.6726 14.8613L29.802 21.9907L34.7271 17.0656L39.6522 21.9907L40.9848 20.6582V37.1237H4.01562V8.55664H26.2181Z" fill="#DDDDDD"/>
+          <path d="M28.6133 3.82275L40.84 16.0495" stroke="#666666" stroke-width="3.67"/>
+          <path d="M28.6133 16.0495L40.84 3.82275" stroke="#666666" stroke-width="3.67"/>
+          <path d="M40.9848 20.3196V37.1237H4.01562V8.55664H24.1806" stroke="#666666" stroke-width="3.67"/>
+          <path d="M34.2636 30.4023H20.8203" stroke="#666666" stroke-width="3.67"/>
+          <path d="M17.458 30.4023H10.7363" stroke="#666666" stroke-width="3.67"/>
+          </svg>`;
+                // Add icon inside label
+                label === null || label === void 0 ? void 0 : label.prepend(icon);
             });
         }
-
     }
-
     return element;
 }
