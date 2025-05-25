833123897db661083a765087ec9bd3fe8a08d3d5
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-25 21:29:56 +1200

    nj

--- blocks/form/components/repeatable-panel/repeatable-panel.js
+++ blocks/form/components/repeatable-panel/repeatable-panel.js
@@ -1,10 +1,13 @@
-import { RepeatablePanelFactory } from `${window.hlx.codeBasePath}/blocks/form/components/repeatable-panel/factory.js`
 
 export default async function decorate(el, field) {
 
     const targetNode = document.querySelector('.form.block');
 
     if (targetNode) {
+        const RepeatablePanelFactory = await import(
+            `${window.hlx.codeBasePath}/blocks/form/components/repeatable-panel/factory.js`
+        )
+
         // Create a MutationObserver instance
         const observer = new MutationObserver((mutationsList) => {
             for (const mutation of mutationsList) {
@@ -20,6 +23,7 @@ export default async function decorate(el, field) {
                     const newValue = targetNode.getAttribute('data-block-status');
                     if (newValue === 'loaded') {
                         // Implement repeatable panel customisations
+
                         RepeatablePanelFactory.createRepeatablePanel(el, field);
 
                         // Stop observing as only needed once
