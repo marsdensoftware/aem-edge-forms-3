f53dece9b8f0e4837fa7914b24d801766bf0eb1a
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-26 20:35:56 +1200

    feat(form) changed code to use promise

--- blocks/form/components/education/education.js
+++ blocks/form/components/education/education.js
@@ -1,27 +1,29 @@
 import { EducationRepeatable } from "./educationrepeatable.js";
 
-function onElementAdded(el, callback) {
-    if (el.isConnected) {
-        callback(el);
-        return;
-    }
-
-    const observer = new MutationObserver(() => {
+function onElementAdded(el) {
+    return new Promise((resolve) => {
         if (el.isConnected) {
-            observer.disconnect();
-            callback(el);
+            resolve(el);
+            return;
         }
-    });
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
+        const observer = new MutationObserver(() => {
+            if (el.isConnected) {
+                observer.disconnect();
+                resolve(el);
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
     });
 }
 
 export default async function decorate(el) {
 
-    onElementAdded(el, () => {
+    onElementAdded(el).then(() => {
         const repeatablePanel = el.querySelector('.repeat-wrapper');
         const obj = new EducationRepeatable(repeatablePanel);
 
