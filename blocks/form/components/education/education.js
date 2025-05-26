31496756bd91e281d2f268d320afaabaf8cf5ca6
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-26 17:48:29 +1200

    nj

--- blocks/form/components/education/education.js
+++ blocks/form/components/education/education.js
@@ -1,11 +1,32 @@
 import { EducationRepeatable } from "./educationrepeatable.js";
 
+function onElementAdded(el, callback) {
+    if (el.isConnected) {
+        callback(el);
+        return;
+    }
+
+    const observer = new MutationObserver(() => {
+        if (el.isConnected) {
+            observer.disconnect();
+            callback(el);
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+}
+
 export default async function decorate(el) {
 
-    const repeatablePanel = el.querySelector('[name="education-options-content"]');
+    onElementAdded(el, () => {
+        const repeatablePanel = el.querySelector('.repeat-wrapper');
+        const obj = new EducationRepeatable(repeatablePanel);
 
-    const obj = new EducationRepeatable(repeatablePanel);
-    obj.init();
+        obj.init();
+    });
 
     return el;
 }
