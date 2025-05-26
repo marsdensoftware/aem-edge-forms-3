d9e1da7c96bdcb23fd88bbb42e76d64718604712
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-26 17:42:35 +1200

    nj

--- blocks/form/components/education/education.js
+++ blocks/form/components/education/education.js
@@ -2,7 +2,7 @@ import { EducationRepeatable } from "./educationrepeatable.js";
 
 export default async function decorate(el) {
 
-    const repeatablePanel = el.querySelector('name=[".education-options-content"]');
+    const repeatablePanel = el.querySelector('[name="education-options-content"]');
 
     const obj = new EducationRepeatable(repeatablePanel);
     obj.init();
