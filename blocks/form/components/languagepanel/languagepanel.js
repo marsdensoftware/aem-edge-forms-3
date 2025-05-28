995c4505d8a047901da03b378e7d1e3a7d2905a1
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-28 16:57:47 +1200

    nj

--- blocks/form/components/languagepanel/languagepanel.js
+++ blocks/form/components/languagepanel/languagepanel.js
@@ -1,11 +1,11 @@
-import { LanguageRepeatable } from './languagepanelrepeatable.js';
+import { LanguagePanelRepeatable } from './languagepanelrepeatable.js';
 import { onElementAdded } from '../utils.js'
 
 export default async function decorate(el) {
 
     onElementAdded(el, () => {
         const repeatablePanel = el.querySelector('.repeat-wrapper');
-        const obj = new LanguageRepeatable(repeatablePanel);
+        const obj = new LanguagePanelRepeatable(repeatablePanel);
 
         obj.init();
     });
