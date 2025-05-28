e9567638b43ec40f5082dec52d7ace327eb4f712
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-05-28 17:16:10 +1200

    nj

--- blocks/form/components/languagepanel/languagerepeatable.js
+++ blocks/form/components/languagepanel/languagerepeatable.js
@@ -3,6 +3,6 @@ import { ConditionalRepeatable } from "../repeatable-panel/default/default.js";
 export class LanguagePanelRepeatable extends ConditionalRepeatable {
 
     constructor(repeatablePanel) {
-        super(repeatablePanel, 'language');
+        super(repeatablePanel, 'languagepanel');
     }
 }
