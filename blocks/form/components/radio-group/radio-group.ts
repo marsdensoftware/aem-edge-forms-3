8dcae26832915c56cf7c74e4298adf98955820b1
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-06-05 12:27:37 +1200

    nj

--- blocks/form/components/radio-group/radio-group.ts
+++ blocks/form/components/radio-group/radio-group.ts
@@ -1,35 +1,35 @@
 interface Field {
-  properties: {
-    withIcon: boolean
-    enumIconNames: string[]
-  }
+    properties: {
+        withIcon: boolean
+        enumIconNames: string[]
+    }
 }
 
 export default function decorate(element: Element, field: Field) {
-  const { withIcon, enumIconNames } = field.properties
+    const { withIcon, enumIconNames } = field.properties
 
-  if (withIcon) {
-    element.classList.add('radio-group--with-icon')
-    element.classList.remove('horizontal', 'vertical')
-    
-    const divs = element.querySelectorAll('div')
+    if (withIcon) {
+        element.classList.remove('horizontal', 'vertical')
+        element.classList.add('radio-group--with-icon')
 
-    if (divs.length) {
-      enumIconNames.forEach((iconName, index) => {
-        const div = divs[index]
-        // LABEL
-        const label = div.querySelector('label')
-        label?.classList.add('radio-group--with-icon__label')
+        const divs = element.querySelectorAll('div')
 
-        // ICON
-        const icon = document.createElement('span')
-        icon.className = `radio-group--with-icon__icon radio-group--with-icon--${iconName}`
+        if (divs.length) {
+            enumIconNames.forEach((iconName, index) => {
+                const div = divs[index]
+                // LABEL
+                const label = div.querySelector('label')
+                label?.classList.add('radio-group--with-icon__label')
 
-        // Add icon inside label
-        label?.prepend(icon)
-      })
+                // ICON
+                const icon = document.createElement('span')
+                icon.className = `radio-group--with-icon__icon radio-group--with-icon--${iconName}`
+
+                // Add icon inside label
+                label?.prepend(icon)
+            })
+        }
     }
-  }
 
-  return element
+    return element
 }
