be51b6a05d89aecd1c2ded365dcb947171f06e93
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-06-06 08:36:14 +1200

    nj

--- blocks/form/components/datepicker/datepicker.js
+++ blocks/form/components/datepicker/datepicker.js
@@ -1,89 +1,77 @@
-export function handleTabNavigation(panel, index) {
-    const tabs = panel.querySelectorAll(':scope > fieldset');
-    const navItems = panel.querySelectorAll(':scope > .navitems > li');
-    tabs.forEach((otherTab, i) => {
-        var _a, _b;
-        if (i === index) {
-            otherTab.classList.add('formtab-current');
-            (_a = navItems[i]) === null || _a === void 0 ? void 0 : _a.classList.add('navitem-current');
-        }
-        else {
-            otherTab.classList.remove('formtab-current');
-            (_b = navItems[i]) === null || _b === void 0 ? void 0 : _b.classList.remove('navitem-current');
-        }
-    });
-}
-export default function decorate(panel, model) {
-    function parseNumber(str, defaultValue = 0) {
+class DatepickerField {
+    constructor(panel, model) {
+        this.panel = panel;
+        this.model = model;
+        this.currentYear = new Date().getFullYear();
+        this.defaultYearMin = this.currentYear - 20;
+        this.defaultYearMax = this.currentYear;
+
+        this.yearDD = panel.querySelector('[name="year"]');
+        this.monthDD = panel.querySelector('[name="month"]');
+
+        this.init();
+    }
+
+    parseNumber(str, defaultValue = 0) {
         const num = Number(str);
         return isNaN(num) ? defaultValue : num;
     }
 
-    function plusMinus(n) {
-        return n && (n.charAt(0) == '+' || '' + yearMinS.charAt(0) == '-');
+    plusMinus(str) {
+        return str && (str.charAt(0) === '+' || str.charAt(0) === '-');
     }
 
-    function setDDRequired(dd, required) {
-        dd.parentElement.dataset.required = required;
-        if (required) {
-            dd.setAttribute('required', 'required');
+    init() {
+        this.panel.querySelector('legend')?.classList.add('p-large');
+        this.panel.dataset.yearMax = this.model.properties.yearMax;
+        this.panel.dataset.yearMin = this.model.properties.yearMin;
+        this.panel.dataset.required = this.yearDD.required || this.monthDD.required;
+
+        let yearMinS = this.model.properties.yearMin;
+        let yearMaxS = this.model.properties.yearMax;
+
+        let yearMin = this.parseNumber(yearMinS, this.defaultYearMin);
+        let yearMax = this.parseNumber(yearMaxS, this.defaultYearMax);
+
+        if (this.plusMinus(yearMinS)) {
+            yearMin = this.currentYear + parseInt(yearMinS);
         }
-        else {
-            dd.removeAttribute('required');
+
+        if (this.plusMinus(yearMaxS)) {
+            yearMax = this.currentYear + parseInt(yearMaxS);
         }
+
+        this.populateYears(yearMin, yearMax);
+        this.populateMonths();
     }
 
-    panel.querySelector('legend')?.classList.add('p-large');
-
-    const yearDD = panel.querySelector('[name="year"]');
-    const monthDD = panel.querySelector('[name="month"]');
-    const required = model.properties.fieldRequired;
-
-    panel.dataset.required = required;
-    setDDRequired(yearDD, required);
-    setDDRequired(monthDD, required);
-
-    const currentYear = new Date().getFullYear();
-    const defaultYearMin = currentYear - 20;
-    const defaultYearMax = currentYear;
-
-    let yearMax = parseNumber(model.properties.yearMax, defaultYearMax);
-    let yearMin = parseNumber(model.properties.yearMin, defaultYearMin);
-
-    let yearMinS = model.properties.yearMin;
-    let yearMaxS = model.properties.yearMax;
-
-    if (plusMinus(yearMinS)) {
-        yearMin = currentYear + parseInt(yearMinS)
-    }
-
-    if (plusMinus(yearMaxS)) {
-        yearMax = currentYear + parseInt(yearMaxS)
-    }
-
-    // Populate year if empty
-
-    let options = yearDD.querySelectorAll('option:not([disabled])');
-    if (options.length === 0) {
-        for (let year = yearMin; year <= yearMax; year++) {
-            const option = document.createElement('option');
-            option.value = year;
-            option.textContent = year;
-            yearDD.appendChild(option);
+    populateYears(yearMin, yearMax) {
+        const options = this.yearDD.querySelectorAll('option:not([disabled])');
+        if (options.length === 0) {
+            for (let year = yearMin; year <= yearMax; year++) {
+                const option = document.createElement('option');
+                option.value = year;
+                option.textContent = year;
+                this.yearDD.appendChild(option);
+            }
         }
     }
 
-    // Populate months if empty
-
-    options = monthDD.querySelectorAll('option:not([disabled])');
-    if (options.length === 0) {
-        for (let month = 1; month <= 12; month++) {
-            const option = document.createElement('option');
-            option.value = month;
-            option.textContent = month;
-            monthDD.appendChild(option);
+    populateMonths() {
+        const options = this.monthDD.querySelectorAll('option:not([disabled])');
+        if (options.length === 0) {
+            for (let month = 1; month <= 12; month++) {
+                const option = document.createElement('option');
+                option.value = month;
+                option.textContent = month;
+                this.monthDD.appendChild(option);
+            }
         }
     }
+}
 
+// Public method to decorate a single panel
+export function decorate(panel, model) {
+    new DatepickerField(panel, model);
     return panel;
 }
