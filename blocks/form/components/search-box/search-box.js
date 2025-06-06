5229cb5fba88aeeebee7c0179f176c020c46e715
gking005cladmin <gking005cladmin@msd.govt.nz>
2025-06-06 12:33:28 +1200

    sepd943 - update searchbox componentto use typeahead

--- blocks/form/components/search-box/search-box.js
+++ blocks/form/components/search-box/search-box.js
@@ -1,10 +1,9 @@
+import typeaheadDecorator from '../typeahead/typeahead.js';
 
-import { decorate as typeaheadDecorator } from '../typeahead/typeahead.js';
-
-export function decorate(element, field) {
+export default function decorate(element, field) {
     // Add search-box specific class
     element.classList.add('search-box');
-    
+
     // Use typeahead decorator for rendering if specified
     if (field.properties['fd:render'] === 'typeahead') {
         typeaheadDecorator(element, field);
