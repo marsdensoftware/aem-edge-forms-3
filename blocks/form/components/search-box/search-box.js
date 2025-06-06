35ca315d16700d8ef5cec2806983f837d78c44b6
gking005cladmin <gking005cladmin@msd.govt.nz>
2025-06-06 14:32:51 +1200

    sepd943 - update searchbox component to use typeahead

--- blocks/form/components/search-box/search-box.js
+++ blocks/form/components/search-box/search-box.js
@@ -106,25 +106,6 @@ document.addEventListener('input', (event) => {
     }
 });
 
-function addSelectedResultsDiv(field) {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('selected-results-wrapper');
-
-    // Add the label if it's specified in the properties
-    if (field.properties.resultsLabel) {
-        const label = document.createElement('div');
-        label.classList.add('selected-results-label');
-        label.textContent = field.properties.resultsLabel;
-        wrapper.appendChild(label);
-    }
-
-    const resultsDiv = document.createElement('div');
-    resultsDiv.classList.add('selected-results');
-    wrapper.appendChild(resultsDiv);
-
-    return wrapper;
-}
-
 export default function decorate(element, field, container) {
     const datasource = field.properties.datasource;
 
@@ -133,11 +114,11 @@ export default function decorate(element, field, container) {
 
     // Add suggestion div
     const suggestionsDiv = addSuggestionDiv();
-    element.append(suggestionsDiv);
+    element.appendChild(suggestionsDiv);
 
-    // Add selected results div with label
-    const selectedResultsDiv = addSelectedResultsDiv(field);
-    element.append(selectedResultsDiv);
+    // Add selected cards container
+    const selectedCardsDiv = addSelectedCardsDiv();
+    element.appendChild(selectedCardsDiv);
 
     return element;
 }
