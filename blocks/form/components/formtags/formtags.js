b7add02e51c1c4c153d66135762e22ab03df32bc
Seno <aseno001cladmin@msd.govt.nz>
2025-06-06 14:22:12 +1200

    feat(form): tags components

--- blocks/form/components/formtags/formtags.js
+++ blocks/form/components/formtags/formtags.js
@@ -1,5 +1,25 @@
 export default function decorate(panel) {
-    console.log(panel);
-    console.log('asdasdsadsad!!!!!');
     panel.classList.add('tags');
+    const childInput = panel.querySelectorAll('input');
+    const childDivs = panel.querySelectorAll('div');
+    childInput[0].remove();
+    childInput[1].remove();
+    const text = document.createElement('span');
+    text.className = 'tags-item__title';
+    text.innerHTML = 'Porirua, Wellington';
+    const closeButton = document.createElement('span');
+    closeButton.className = 'tags-item__close';
+    closeButton.innerHTML = 'x';
+    const text2 = document.createElement('span');
+    text2.className = 'tags-item__title';
+    text2.innerHTML = 'Newmarket, Auckland';
+    const closeButton2 = document.createElement('span');
+    closeButton2.className = 'tags-item__close';
+    closeButton2.innerHTML = 'x';
+    childDivs.forEach((div) => {
+        div.classList.add('tags__item');
+        div.classList.remove('text-wrapper');
+    });
+    childDivs[0].append(text, closeButton);
+    childDivs[1].append(text2, closeButton2);
 }
