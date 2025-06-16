44f487766d1c74eb2622939dde05e592d0f1675b
Seno <aseno001cladmin@msd.govt.nz>
2025-06-16 15:43:20 +1200

    style(grid): initiate grid on first screen

--- blocks/form/components/formhero/formhero.ts
+++ blocks/form/components/formhero/formhero.ts
@@ -1,10 +1,21 @@
 export default function decorate(panel: Element) {
-  panel.classList.add('panel-formhero')
+  panel.classList.add('panel-formhero', 'container-xl')
 
   const divs = panel.querySelectorAll('div')
 
-  divs[0].classList.add('panel-formhero__content')
-  divs[1].classList.add('panel-formhero__picture')
+  divs[0].classList.add(
+    'panel-formhero__content',
+    'col-12',
+    'col-lg-5',
+    'offset-lg-2',
+  )
+
+  divs[1].classList.add('panel-formhero__picture', 'col-12', 'col-md-4')
+
+  const row = document.createElement('section')
+  panel.appendChild(row)
+  row.classList.add('row', 'panel-formhero__inner')
+  row.append(divs[0], divs[1])
 
   return panel
 }
