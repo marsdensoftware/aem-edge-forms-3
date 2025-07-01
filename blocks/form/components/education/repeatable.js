a5c6d921666eb831c9e932c6b1a3da9188bcecfc
Greg Chalmers <gregory.charles.chalmers@gmail.com>
2025-07-01 17:30:02 +1200

    adds month + year to education form

--- blocks/form/components/education/repeatable.js
+++ blocks/form/components/education/repeatable.js
@@ -1,57 +1,57 @@
-import { ConditionalRepeatable } from "../repeatable-panel/default/default.js";
+import { ConditionalRepeatable } from '../repeatable-panel/default/default.js'
 import { FIELD_NAMES } from './fieldnames.js'
 import { DefaultFieldConverter } from '../utils.js'
 
 class Converter extends DefaultFieldConverter {
+  convert(element) {
+    const result = super.convert(element)
 
-    convert(element) {
-        const result = super.convert(element);
-
-        // Customize rendering for completion-year, completion status
-        const completionStatus = result[FIELD_NAMES.COMPLETION_STATUS];
-        if (completionStatus?.value == '0') {
-            // Completed
-            const year = result[FIELD_NAMES.FINISH_YEAR];
-            completionStatus.displayValue += ` ${year.displayValue}`;
-        }
-
-        // Delete start and finish
-        delete result[FIELD_NAMES.FINISH_YEAR];
-        delete result[FIELD_NAMES.START_YEAR];
-
-        return result;
+    // Customize rendering for completion-year, completion status
+    const completionStatus = result[FIELD_NAMES.COMPLETION_STATUS]
+    if (completionStatus?.value == '0') {
+      // Completed
+      const year = result[FIELD_NAMES.FINISH_YEAR]
+      completionStatus.displayValue += ` ${year.displayValue}`
     }
+
+    // Delete start and finish
+    delete result[FIELD_NAMES.FINISH_MONTH]
+    delete result[FIELD_NAMES.FINISH_YEAR]
+    delete result[FIELD_NAMES.START_MONTH]
+    delete result[FIELD_NAMES.START_YEAR]
+
+    return result
+  }
 }
 
 export class EducationRepeatable extends ConditionalRepeatable {
+  constructor(repeatablePanel, properties) {
+    super(repeatablePanel, properties, 'education', new Converter())
+  }
 
-    constructor(repeatablePanel, properties) {
-        super(repeatablePanel, properties, 'education', new Converter());
-    }
+  _init(entry) {
+    super._init(entry)
+    // Register listener on completion status
+    const completionStatusRadios = entry.querySelectorAll(
+      `input[name="${FIELD_NAMES.COMPLETION_STATUS}"]`,
+    )
+    const panel = entry.querySelector(
+      `[name="${FIELD_NAMES.FINISH_DATEPICKER}"]`,
+    )
+    // Defaults to hidden, as there is no option of this in
+    // UE for advanced date picker.
+    panel.setAttribute('data-visible', false)
 
-    _init(entry) {
-        super._init(entry);
-        // Register listener on completion status
-        const completionStatusRadios = entry.querySelectorAll(`input[name="${FIELD_NAMES.COMPLETION_STATUS}"]`);
-        const finishYear = entry.querySelector(`.field-${FIELD_NAMES.FINISH_YEAR}`);
+    completionStatusRadios.forEach((radio) => {
+      radio.addEventListener('change', () => {
+        panel.setAttribute('data-visible', radio.value === '0')
+      })
+    })
+  }
 
-        completionStatusRadios.forEach(radio => {
-            radio.addEventListener('change', () => {
-                if (radio.value == '0') {
-                    // Completed, show finish year
-                    finishYear.setAttribute('data-visible', true);
-                }
-                else {
-                    // Not completed, hide finish year
-                    finishYear.setAttribute('data-visible', false);
-                }
-            });
-        });
-    }
+  _onItemAdded(entry) {
+    this._init(entry)
 
-    _onItemAdded(entry) {
-        this._init(entry);
-
-        super._onItemAdded(entry);
-    }
+    super._onItemAdded(entry)
+  }
 }
