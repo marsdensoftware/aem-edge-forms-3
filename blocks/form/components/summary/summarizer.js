6805a70e1f3212cf6840f2041f0b05cb093ec503
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-07-08 10:13:18 +1200

    feat(form) education bugfix

--- blocks/form/components/summary/summarizer.js
+++ blocks/form/components/summary/summarizer.js
@@ -57,12 +57,12 @@ class EducationConverter {
 
         // Customize rendering for completion-year, completion status
         const completionStatus = result[EducationFieldNames.COMPLETION_STATUS];
-        if (completionStatus?.value == '0') {
+        if (completionStatus?.value == 0) {
             // Completed
             const endYear = result[EducationFieldNames.FINISH_YEAR];
             summary.push(`${i18n('Finished')} ${endYear.displayValue}`);
         }
-        else if(completionStatus?.value == '1') {
+        else if(completionStatus?.value == 1) {
             // In progress, partially completed
             summary.push(`${i18n('Started')} ${startYear.displayValue}`, `${i18n('Partially complete')}`);
         }
