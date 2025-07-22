3034c322911eb7f17695b6255f377e227c7561de
gking005cladmin <gking005cladmin@msd.govt.nz>
2025-07-22 13:06:41 +1200

    sepd-996 add icon support to extended checkbox

--- blocks/form/components/extended-checkbox/extended-checkbox.js
+++ blocks/form/components/extended-checkbox/extended-checkbox.js
@@ -1,3 +1,16 @@
 export default function decorate(fieldDiv, fieldJson) {
-  console.log('hi from extended checkbox group');
+    console.log('hi from extended checkbox group');
+    const { iconName } = fieldJson.properties;
+    // if we have an iconName, then add the extended-checkbox--icon-${iconName} class to the fieldDiv
+    if (iconName) {
+        fieldDiv.classList.add(`extended-checkbox--icon-${iconName}`);
+    }
+    // set up the structure of the component as:
+    // <div class="checkbox-wrapper field-main-checkbox field-wrapper" data-id="checkbox-616739e2ec" data-required="false" data-component-status="loaded" data-active="true">
+    // <input type="checkbox" value="on" id="checkbox-616739e2ec" name="main-checkbox" autocomplete="off">
+    //     <label for="checkbox-616739e2ec" class="field-label">Checkbox Title</label>
+    // <hr class="checkbox-divider">
+    // <textarea class="checkbox-description" placeholder="Enter description here">Copy here explaining an example of this skill etc over multiple lines as required. Copy here explaining an example of this skill etc over multiple lines as required.</textarea>
+    // </div>
+    return fieldDiv;
 }
