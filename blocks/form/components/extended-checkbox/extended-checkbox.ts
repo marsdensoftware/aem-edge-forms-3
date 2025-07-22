4ddab30c4b2bb784c29b5c93dd20963cbf8e0664
gking005cladmin <gking005cladmin@msd.govt.nz>
2025-07-22 13:21:08 +1200

    sepd-996 add hr and description to extended checkbox

--- blocks/form/components/extended-checkbox/extended-checkbox.ts
+++ blocks/form/components/extended-checkbox/extended-checkbox.ts
@@ -16,15 +16,16 @@ export default function decorate(fieldDiv: Element, fieldJson: Field) {
     fieldDiv.classList.add(`extended-checkbox--icon-${iconName}`);
   }
 
-  // set up the structure of the component as:
-  // <div class="checkbox-wrapper field-main-checkbox field-wrapper" data-id="checkbox-616739e2ec" data-required="false" data-component-status="loaded" data-active="true">
-  // <input type="checkbox" value="on" id="checkbox-616739e2ec" name="main-checkbox" autocomplete="off">
-  //     <label for="checkbox-616739e2ec" class="field-label">Checkbox Title</label>
-  // <hr class="checkbox-divider">
-  // <textarea class="checkbox-description" placeholder="Enter description here">Copy here explaining an example of this skill etc over multiple lines as required. Copy here explaining an example of this skill etc over multiple lines as required.</textarea>
-  // </div>
+  //create the divider
+  const divider = document.createElement('hr')
+  divider.classList.add('checkbox-divider')
 
+  const textArea = document.createElement('textarea')
+  textArea.classList.add('checkbox-description')
+  textArea.placeholder = 'Enter description here'
+  textArea.value = 'Copy here explaining an example of this skill etc over multiple lines as required. Copy here explaining an example of this skill etc over multiple lines as required.'
 
+  fieldDiv.append(divider, textArea)
 
   return fieldDiv;
 
