cf756fa6d9a8ba121ab1330b2feeba59af38ea31
Oscar Key <oscar@ghostdynamics.co>
2025-07-23 11:56:39 +1200

    dev: extract fields

--- blocks/list/list.js
+++ blocks/list/list.js
@@ -1,3 +1,10 @@
 export default function decorate(block) {
   console.log('decorating', block);
+
+  console.log(`got source`,  document.querySelector('[data-aue-prop="source"]'));
+  console.log(`got offset-arg`,  document.querySelector('[data-aue-prop="offset-arg"]'));
+  console.log(`got page-size-arg`,  document.querySelector('[data-aue-prop="page-size-arg"]'));
+  console.log(`got page-size`,  document.querySelector('[data-aue-prop="page-size"]'));
+  console.log(`got select-output`,  document.querySelector('[data-aue-prop="select-output"]'));
+  /*select by data-aue-prop= like document.querySelector('[data-aue-prop="14"]');*/
 }
