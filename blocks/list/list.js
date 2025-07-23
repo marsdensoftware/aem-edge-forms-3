86dec75eb478d3849ab3d4714b6249e0eddfffef
Oscar Key <oscar@ghostdynamics.co>
2025-07-23 16:39:38 +1200

    dev: hook up pager

--- blocks/list/list.js
+++ blocks/list/list.js
@@ -1,4 +1,4 @@
-import { PagerConfig } from './scroll.js';
+import { Pager, PagerConfig } from './scroll.js';
 import { validUrl } from './utils.js';
 
 function dataProp(block, name) {
@@ -34,9 +34,37 @@ export default function decorate(block) {
   const config = configFromFields(block);
   console.log('extracted config', config);
 
+  const pager = Object.create(Pager);
+  pager.config = config;
+  // TODO load offset/in_progress from query params
+
   const button = document.createElement('button');
   button.innerText = 'Click here';
-  button.onclick = function() {console.log('clicked');};
-  button.className = 'hack'; // TODO FIXME temp
   block.replaceChildren(button);
+
+  const wrapper = document.createElement('div');
+  block.append(wrapper);
+
+  pager.up_observer = makePageLoadObserver(wrapper, pager, true); // TODO need to bind this to an element after first load -- maybe separate .observe() logic from the rest?
+  pager.down_observer = makePageLoadObserver(wrapper, pager, false);
+  pager.pos_observer = makePositionObserver(pager);
+
+  button.onclick = function() {
+    if (pager.loading) {
+      return;
+    }
+
+    console.log('clicked');
+    button.onclick = function(){};
+    button.disabled = true;
+
+    pager.loading = 'down'; // TODO not invasive
+    const cards = await loadPage(wrapper, pager, pager.config.offset);
+    if (cards) {
+      pager.top = cards[0];
+      pager.up_observer.observe(pager.top);
+    }
+  };
+  button.className = 'hack'; // TODO FIXME temp
+
 }
