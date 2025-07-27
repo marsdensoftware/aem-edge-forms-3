6ae971e42b6d0cad2a349bae94b7656a9d903d12
Greg Chalmers <gregory.charles.chalmers@gmail.com>
2025-07-27 12:23:45 +1200

    trying to work out away to know if script in UE

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,4 +1,14 @@
 // add delayed functionality here
 //
+//
+console.log('Delayed started')
+for (const m of document.querySelectorAll('main')) {
+  for (const a of m.attributes) {
+    console.log(a)
+    console.log(a.name)
+    console.log(a.value)
+  }
+}
+console.log('Delayed ended')
 //import { authStartKeepAlive } from './auth.js'
 //authStartKeepAlive()
