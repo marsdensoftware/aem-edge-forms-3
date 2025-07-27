852f499b3eef18084af65778ca86eaff0b970962
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-07-27 13:58:31 +1200

    feat(content) link with icon

--- blocks/utils.js
+++
@@ -1,4 +0,0 @@
-export function getOptions(block, blockName) {
-  // Get the block's classes, excluding 'block' and 'blockName'; anything remaining is a block option.
-  return [...block.classList].filter((c) => !['block', `${blockName}`].includes(c));
-}
