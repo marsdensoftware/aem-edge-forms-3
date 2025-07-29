e3042890153236a90f57e36f39f209abde45e969
Greg Chalmers <gregory.charles.chalmers@gmail.com>
2025-07-29 15:12:12 +1200

    rm changes I dont want

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,1 @@
-// import { authStartKeepAlive } from './msd/auth.js'
-
-const isEditor = () =>
-  document.querySelector('main[data-aue-resource],main[data-aue-label]') !==
-  null
-
-console.log('hello delayed script', { isEditor: isEditor() })
+// add delayed functionality here
