a9502e17ad6247894400ee10349c242f6e299f83
Oscar Key <oscar@ghostdynamics.co>
2025-07-29 16:40:01 +1200

    dev: add creation for example list item

--- blocks/person/person.js
+++ blocks/person/person.js
@@ -1,2 +1,19 @@
+export function create(data) {
+  const item = document.createElement('div');
+  // TODO codegen
+  const fields = {
+    example_id: data.id,
+    firstname: data.firstName,
+    surname: data.lastName,
+  };
+  Object.entries(fields)
+    .forEach(([key, value]) => {
+      const field = document.createElement('span');
+      field.innerText = `${key}: ${typeof value !== 'object' ? value : JSON.stringify(value)}`;
+      item.append(field);
+    });
+
+  return item;
+}
 export default function decorate(block) {
 };
