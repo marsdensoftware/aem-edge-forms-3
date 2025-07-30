c6a4021e0c766d7734aca4afbc0ef6f47a59f5bb
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-07-30 13:26:39 +1200

    Update formoutputfield.js

--- blocks/form/components/formoutputfield/formoutputfield.js
+++ blocks/form/components/formoutputfield/formoutputfield.js
@@ -1,34 +1,48 @@
+import { onElementAdded } from '../utils.js'
 
-function waitForWindowObject(key) {
-    return new Promise((resolve) => {
-        const check = () => {
-            if (window[key]) {
-                resolve(window[key]);
-            } else {
-                setTimeout(check, 100); // check again in 100ms
-            }
-        };
-
-        check();
-    });
-}
-
-export default function decorate(fieldDiv, fieldJson) {
-    // get the input element from the fieldDiv
-    waitForWindowObject('myForm').then((form) => {
-        console.log('Form is available:', form);
-        const field = form.getElement(fieldJson.id);
-        if (field) {
-            // Subscribe to field change event
-            field.subscribe((payload) => {
-                const { changes, field: fieldModel } = payload;
-                const {
-                    id, name, fieldType, ':type': componentType, readOnly, type, displayValue, displayFormat, displayValueExpression,
-                    activeChild,
-                } = fieldModel;
-            }, 'fieldChanged');
-        }
+const renderers = {
+  'list': function(values) {
+    const result = document.createElement('ul');
+    values.forEach(value => {
+      const li = document.createElement('li');
+      li.textContent = value;
+      result.appendChild(li);
     });
 
-    return fieldDiv;
+    return result;
+  }
+};
+
+export default function decorate(el, fd) {
+  const titleEl = document.createElement('h6');
+  const { fieldName, title, renderer = 'list' } = fd.properties;
+  const renderFunction = renderers['renderer'];
+
+  titleEl.innerHTML = title;
+
+  onElementAdded(el).then((connectedEl) => {
+
+    const form = connectedEl.closest('form');
+
+    // Listen for input events on the form (event delegation)
+    form.addEventListener('change', function(e) {
+      if (e.target.name == fieldName) {
+        updateDisplay();
+      }
+    });
+
+    // Function to update the display
+    function updateDisplay() {
+      const values = Array.from(form.querySelectorAll(`input[name="${fieldName}"]`))
+        .map(input => input.value.trim())
+        .filter(value => value !== '');
+
+      const output = renderFunction(values);
+
+      el.innerHTML = output;
+    }
+  });
+
+  return el;
 }
+
