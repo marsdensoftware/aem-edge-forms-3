23d50bbdb1bb201a064a732ed24fab4acae9abeb
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-08-05 13:49:49 +1200

    feat(footer) link with icon no-icon class

--- blocks/link-with-icon/link-with-icon.js
+++ blocks/link-with-icon/link-with-icon.js
@@ -2,6 +2,12 @@ export default function decorate(block) {
   const children = [...block.children]
 
   const openInNewTab = children[2]?.textContent.trim()
+  children[0]?.classList.add('icon')
+  const icon = children[0]?.querySelector('picture')
+
+  if (!icon) {
+    block.classList.add('no-icon');
+  }
 
   if (openInNewTab) {
     children[1]?.querySelector('a')?.setAttribute('target', '_blank')
