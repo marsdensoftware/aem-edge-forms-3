fafb7dd0a53f6fe7f20910ec0b765134c634d716
gking005cladmin <gking005cladmin@msd.govt.nz>
2025-08-05 14:36:49 +1200

    SEPD-996 add wizard next and prev button handlers to hide the toasts

--- blocks/form/components/extended-checkbox-container/extended-checkbox-container.ts
+++ blocks/form/components/extended-checkbox-container/extended-checkbox-container.ts
@@ -183,7 +183,7 @@ export default function decorate(fieldDiv: Element, fieldJson: Field) {
    const wizardButtonWrapper = wizardPanel.querySelector('.wizard-button-wrapper');
    if (!wizardButtonWrapper) return;
 
-   //attach a listener to the wizard-button-next and wizard-button-prev so we close the toast when they are clicked
+   //attach a listener to the wizard-button-next and wizard-button-prev so we hide the toast when they are clicked
    wizardButtonWrapper.addEventListener('click', (event) => {
      const target = event.target as HTMLElement;
      if (target.id === 'wizard-button-next' || target.id === 'wizard-button-prev') {
