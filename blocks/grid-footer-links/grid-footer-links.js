e8bd13db07e7071ade3bd597a313c31b6864b051
Oscar Key <oscar@ghostdynamics.co>
2025-08-08 15:31:16 +1200

    fix: log message

--- blocks/grid-footer-links/grid-footer-links.js
+++ blocks/grid-footer-links/grid-footer-links.js
@@ -1,7 +1,7 @@
 import { injectLinkWithIconClass } from '../../blocks/grid-footer-summary/grid-footer-summary.js';
 
 export default function decorate(block) {
-  console.log('decorate footer split', block.outerHTML);
+  console.log('decorate footer links', block.outerHTML);
   // TODO only the first should be a link, so we need to re-order?
   // extract the first N as splits, and re-insert at end
   const children = [...block.children];
