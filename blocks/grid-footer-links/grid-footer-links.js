cd2a093213f4ffaf4bfedfb8371e1503faf71ef2
Oscar Key <oscar@ghostdynamics.co>
2025-08-08 15:20:45 +1200

    fix: swap grid links and splits

--- blocks/grid-footer-links/grid-footer-links.js
+++ blocks/grid-footer-links/grid-footer-links.js
@@ -1,48 +1,12 @@
 import { injectLinkWithIconClass } from '../../blocks/grid-footer-summary/grid-footer-summary.js';
 
 export default function decorate(block) {
-  console.log('decorate footer links', block.outerHTML);
+  console.log('decorate footer split', block.outerHTML);
+  // TODO only the first should be a link, so we need to re-order?
+  // extract the first N as splits, and re-insert at end
   const children = [...block.children];
-  if (children.length < 4) {
+  if (!children.length) {
     return;
   }
-  const subcols = children.slice(0, 2);
-  const configs = children.slice(2, 4);
-  const links = children.slice(4)
-
-  injectLinkWithIconClass(links);
-
-  for (const config of configs) {
-    block.removeChild(config);
-    console.log("subcol has config", configs.outerHTML);
-  }
-
-  const innerRow = document.createElement('div');
-  innerRow.classList.add('row');
-  for (const subcol of subcols) {
-    block.removeChild(subcol);
-    innerRow.appendChild(subcol);
-    // TODO inject config
-  }
-
-  block.append(innerRow); // wrap and move subcolumns to the end
-  // TODO decorate sub grid? maybe need to wrap in a row?
+  injectLinkWithIconClass(children);
 }
-
-/*
-TODO need a way to hide all but the top link at certain breakpoints?
-TODO also need a way for the first link to be styled larger?
-
-Summary:
-all links are small
-
-Links:
-First link is large
-Rest are small and collapse
-
-Split:
-First link is large
-link-with-icon at top
-then
-splits - subgrid?
- */
