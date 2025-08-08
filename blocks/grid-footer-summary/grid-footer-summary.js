72caa56463646d53e27662a695db9deacd06b1e9
Oscar Key <oscar@ghostdynamics.co>
2025-08-08 13:00:18 +1200

    dev: dynamically inject link with icon class

--- blocks/grid-footer-summary/grid-footer-summary.js
+++ blocks/grid-footer-summary/grid-footer-summary.js
@@ -1,3 +1,12 @@
 export default function decorate(block) {
   console.log('decorate footer summary', block.outerHTML);
+  const children = [...block.children];
+  if (children.length < 3) {
+    return;
+  }
+  for (const child of children.slice(2)) {
+    console.log('adding link-with-icon class to', child.outerHTML);
+    child.classList.add('link-with-icon');
+    // TODO nuke the empty div and the link-with-icon div
+  }
 }
