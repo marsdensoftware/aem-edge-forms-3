2c3ef1404fff974b5c1f49d7e9dbed1deb75c102
Seno <aseno001cladmin@msd.govt.nz>
2025-08-08 15:35:18 +1200

    feat(nav): main menu dropdown menu

--- blocks/main-menu/main-menu.js
+++ blocks/main-menu/main-menu.js
@@ -1,3 +1,37 @@
+const dropdownMenu = [
+    {
+        title: 'Kia Ora, Matariki!',
+    },
+    {
+        title: 'Manage Jobseeker Profile',
+    },
+    {
+        title: 'Job Applications',
+    },
+    {
+        title: 'Saved',
+    },
+    {
+        title: 'Your CV',
+    },
+];
+let isAccountMenuOpen = false;
+const handleAccountMenu = (triggerEl) => {
+    const el = triggerEl.target;
+    if (!el) {
+        throw new Error('Can not find trigger element');
+    }
+    const accountMenu = el.nextElementSibling;
+    if (!accountMenu) {
+        throw new Error('Can not find account menu element');
+    }
+    isAccountMenuOpen = !isAccountMenuOpen;
+    if (isAccountMenuOpen) {
+        accountMenu.classList.add('main-menu__account-menu--is-visible');
+        return;
+    }
+    accountMenu.classList.remove('main-menu__account-menu--is-visible');
+};
 function decorate(block) {
     const [mainMenuInner] = block.children;
     mainMenuInner.classList.add('main-menu__inner');
@@ -13,6 +47,16 @@ function decorate(block) {
     mainAction.append(logo);
     const userButton = document.createElement('button');
     userButton.classList.add('main-menu__user-button');
-    mainMenuInner.append(mainAction, userButton);
+    // Account menu
+    const accountMenu = document.createElement('ul');
+    accountMenu.classList.add('main-menu__account-menu');
+    dropdownMenu.forEach((menu) => {
+        const list = document.createElement('li');
+        list.classList.add('main-menu__account-menu-item');
+        list.innerHTML = menu.title;
+        accountMenu.append(list);
+    });
+    userButton.addEventListener('click', handleAccountMenu);
+    mainMenuInner.append(mainAction, userButton, accountMenu);
 }
 export default decorate;
