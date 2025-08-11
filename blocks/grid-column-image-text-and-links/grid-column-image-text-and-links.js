2f5d3db0a5e512ad5c7d2b8f723e7bd33036363e
Oscar Key <oscar.c.key@gmail.com>
2025-08-12 11:31:52 +1200

    fix: add missing common script file, and refactor to handle classes in nested blocks and block items

--- blocks/grid-column-image-text-and-links/grid-column-image-text-and-links.js
+++ blocks/grid-column-image-text-and-links/grid-column-image-text-and-links.js
@@ -1,9 +1,9 @@
-import { injectLinkWithIconClass } from '../../scripts/msd/link-with-icon.js';
+import { injectNestingBugClasses } from '../../scripts/msd/link-with-icon.js';
 
 export default function decorate(block) {
   const children = [...block.children];
   if (children.length < 3) {
     return;
   }
-  injectLinkWithIconClass(children.slice(2), 'link-with-icon-item');
+  injectNestingBugClasses(children.slice(2), 'link-with-icon-item');
 }
