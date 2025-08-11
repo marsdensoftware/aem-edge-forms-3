bfb6098340db10f70e7de21d05997a285f5cee9a
Oscar Key <oscar@ghostdynamics.co>
2025-08-11 15:28:45 +1200

    dev: extend grid subcolumns

--- blocks/grid-column-text-subcolumns/grid-column-text-subcolumns.js
+++ blocks/grid-column-text-subcolumns/grid-column-text-subcolumns.js
@@ -4,27 +4,43 @@ export default function decorate(block) {
   block.classList.add('row');
 
   const children = [...block.children];
-  if (children.length < 4) {
+  if (children.length < 9) {
     return;
   }
-  const subcols = children.slice(0, 2);
-  const configs = children.slice(2, 4);
-  const links = children.slice(4)
+  let nCols = children[4]; // TODO does this come out as an integer?
+  console.log('got nCols', nCols, typeof(nCols));
+  if (nCols < 1 || 4 < nCols) {
+    nCols = 2;
+  }
+  const activeSubcols = children.slice(0, nCols);
+  const inactiveSubcols = children.slice(nCols, 4);
+
+  const activeConfigs = children.slice(5, 5+nCols);
+  const inactiveConfigs = children.slice(5+nCols, 9);
+  const links = children.slice(9)
 
   injectLinkWithIconClass(links);
   for (const link of links) {
     link.classList.add('col-12');
   }
 
-  for (const [index, config] of configs.entries()) {
+  for (const config of inactiveConfigs) {
     block.removeChild(config);
+  }
+  for (const subcol of inactiveSubcols) {
+    block.removeChild(subcol);
+  }
+
+  for (const [index, config] of activeConfigs.entries()) {
+    block.removeChild(config);
+
     const values = config.querySelector('p')?.innerText || '';
     for (const value of values.split(',')) {
       subcols[index].classList.add(value)
     }
   }
 
-  for (const subcol of subcols) {
+  for (const subcol of activeSubcols) {
     block.appendChild(subcol);
   }
 }
