8ddaf8343b750b7251bc49f9b5a5c92faca6d10b
Oscar Key <oscar@ghostdynamics.co>
2025-08-12 18:26:40 +1200

    dev: extend image-text-and-links grid block to allow wrapping links with a display class

--- blocks/grid-column-image-text-and-links/grid-column-image-text-and-links.js
+++ blocks/grid-column-image-text-and-links/grid-column-image-text-and-links.js
@@ -2,8 +2,27 @@ import { injectNestingBugClasses } from '../../scripts/msd/link-with-icon.js';
 
 export default function decorate(block) {
   const children = [...block.children];
-  if (children.length < 3) {
+  if (children.length < 4) {
     return;
   }
-  injectNestingBugClasses(children.slice(2), 'link-with-icon');
+  const links = children.slice(3);
+  injectNestingBugClasses(links, 'link-with-icon');
+
+  const config = (children[2].querySelector('p')?.innerText || '').trim();
+  if (!config) {
+    return;
+  }
+  const wrapper = document.createElement('div');
+  const configClasses = config.split(',');
+  for (const configClass of configClasses) {
+    if (!configClass) {
+      continue;
+    }
+    wrapper.classList.add(configClass);
+  }
+
+  for (const link of links) {
+    wrapper.appendChild(link);
+  }
+  block.appendChild(wrapper);
 }
