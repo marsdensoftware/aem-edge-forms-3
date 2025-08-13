5707cbc9f0a78c0bafc4a6c3ea7a41f2fcf6b493
Oscar Key <oscar@ghostdynamics.co>
2025-08-13 12:04:14 +1200

    dev: allow a link to wrap the image in grid column image text and links block

--- blocks/grid-column-image-text-and-links/grid-column-image-text-and-links.js
+++ blocks/grid-column-image-text-and-links/grid-column-image-text-and-links.js
@@ -2,20 +2,29 @@ import { injectNestingBugClasses, handleOpenInNewTab } from '../../scripts/msd/l
 
 export default function decorate(block) {
   const children = [...block.children];
-  if (children.length < 4) {
+  if (children.length < 5) {
     return;
   }
-  const links = children.slice(3);
+  const links = children.slice(4);
   injectNestingBugClasses(links, 'link-with-icon');
   for (const link of links) {
     handleOpenInNewTab(link);
   }
+  const imgLinkWrapper = children[1];
+  const imgLink = imgLinkWrapper.querySelector('a');
+  const imgWrapper = children[0];
+  const img = imgWrapper.querySelector('picture');
+  if (imgLink && img) {
+    imgLink.appendChild(img);
+    block.replaceChild(imgWrapper, imgLinkWrapper);
+  }
 
-  const config = (children[2].querySelector('p')?.innerText || '').trim();
+  const configWrapper = children[3];
+  const config = (configWrapper.querySelector('p')?.innerText || '').trim();
   if (!config) {
     return;
   }
-  block.removeChild(children[2]);
+  block.removeChild(configWrapper);
   const wrapper = document.createElement('div');
   const configClasses = config.split(',');
   for (const configClass of configClasses) {
