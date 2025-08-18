909599e55ebebd879bf447ff2a4ece521662131e
Oscar Key <oscar@ghostdynamics.co>
2025-08-18 12:41:06 +1200

    chore: autofix typescript lints

--- blocks/form/components/radio-group/radio-group.ts
+++ blocks/form/components/radio-group/radio-group.ts
@@ -6,34 +6,34 @@ interface Field {
 }
 
 export default function decorate(element: Element, field: Field) {
-    const { withIcon, enumIconNames } = field.properties
+  const { withIcon, enumIconNames } = field.properties
 
-    if (withIcon) {
-        element.classList.remove('horizontal', 'vertical')
-        element.classList.add('radio-group--with-icon')
+  if (withIcon) {
+    element.classList.remove('horizontal', 'vertical')
+    element.classList.add('radio-group--with-icon')
 
-        const divs = element.querySelectorAll('div')
+    const divs = element.querySelectorAll('div')
 
-        if (divs.length) {
-            enumIconNames.forEach((iconName, index) => {
-                const div = divs[index]
-                // LABEL
-                const label = div.querySelector('label')
-                if (!label) {
-                    return;
-                }
-
-                label.classList.add('radio-group--with-icon__label')
-
-                // ICON
-                const icon = document.createElement('i')
-                icon.className = `radio-group--with-icon__icon radio-group--with-icon--${iconName}`
-
-                // Add icon inside label as first element
-                label.prepend(icon)
-            })
+    if (divs.length) {
+      enumIconNames.forEach((iconName, index) => {
+        const div = divs[index]
+        // LABEL
+        const label = div.querySelector('label')
+        if (!label) {
+          return;
         }
-    }
 
-    return element
+        label.classList.add('radio-group--with-icon__label')
+
+        // ICON
+        const icon = document.createElement('i')
+        icon.className = `radio-group--with-icon__icon radio-group--with-icon--${iconName}`
+
+        // Add icon inside label as first element
+        label.prepend(icon)
+      })
+    }
+  }
+
+  return element
 }
