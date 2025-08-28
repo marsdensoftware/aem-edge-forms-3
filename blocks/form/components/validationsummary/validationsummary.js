2303fbb67ebb70d2e2244afd234178d11aa69ba8
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-08-28 14:06:22 +1200

    feat(form) Validation initial implementation

--- blocks/form/components/validationsummary/validationsummary.js
+++ blocks/form/components/validationsummary/validationsummary.js
@@ -1,4 +1,4 @@
-import { collectFields, waitForVar } from '../utils.js'
+import { onElementAdded } from '../utils.js'
 import fchDecorate from '../formcontextualhelp/formcontextualhelp.js'
 
 /*eslint-disable*/
@@ -10,38 +10,56 @@ export default function decorate(panelEl, model) {
   // Reuse form contextual help decorate
   fchDecorate(panelEl, model);
 
-  (async () => {
-    const myForm = await waitForVar('myForm');
-    const parent = myForm.getElement(panelEl.dataset.id);
+  onElementAdded(panelEl).then((connectedEl) => {
+    const errorContainer = document.createElement('ul');
+    errorContainer.classList.add('error-container');
+    connectedEl.append(errorContainer);
 
-    if (!parent) {
-      return;
+    function updateVisibility() {
+      const hasInvalidFields = errorContainer.querySelector("li:not(.d-none)") !== null;
+
+      connectedEl.dataset.visible = hasInvalidFields;
     }
 
-    const fields = collectFields(parent);
-    const errorContainer = document.createElement('ul');
-    ul.classList.add('error-container');
-    panelEl.append(errorContainer);
+    // Create an observer
+    const observer = new MutationObserver((mutationsList) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+          const target = mutation.target;
+          const id = target.dataset.id;
+          const field = myForm.getElement(id);
 
-    // Subscribe to invalid/valid events on the field datamodel
-    fields.forEach((field) => {
-      const errorFieldContainer = document.createElement('li');
-      errorFieldContainer.id = field.id;
-      errorFieldContainer.style.display = 'none';
-      errorContainer.append(errorFieldContainer);
+          if (!field) {
+            return;
+          }
 
-      field.subscribe((e) => {
-        errorFieldContainer.innerHTML = `<span class="fieldname">${field.label}<span> field.errorMsg`;
-        errorFieldContainer.style.display = 'block';
-      }, 'invalid');
+          let errorFieldContainer = errorContainer.querySelector(`li.error-${id}`);
 
-      field.subscribe((e) => {
-        errorFieldContainer.innerHTML = '';
-        errorFieldContainer.style.display = 'none';
-      }, 'valid');
+          if (target.classList.contains('field-invalid')) {
+            if (!errorFieldContainer) {
+              errorFieldContainer = document.createElement('li');
+              errorFieldContainer.classList.add(`error-${id}`);
+              errorFieldContainer.innerHTML = `<span class="fieldname">${field.label.value}<span> ${field.errorMessage}`;
+              errorContainer.append(errorFieldContainer);
+            }
+
+            errorFieldContainer.classList.remove('d-none');
+          }
+          else {
+            errorFieldContainer?.classList.add('d-none');
+          }
+          updateVisibility();
+        }
+      }
     });
 
-  })();
+    // Start observing for attribute changes
+    observer.observe(connectedEl.parentElement, {
+      attributes: true,
+      attributeFilter: ['class'],
+      subtree: true
+    });
+  });
 
   return panelEl;
 }
