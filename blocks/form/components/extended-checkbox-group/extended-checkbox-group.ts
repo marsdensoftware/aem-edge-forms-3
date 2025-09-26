c2187665a02504437bdd64799759ddac42ec4639
Nono Junang <njuna001cladmin@msd.govt.nz>
2025-09-26 19:37:18 +1200

    feat(form) validation

--- blocks/form/components/extended-checkbox-group/extended-checkbox-group.ts
+++ blocks/form/components/extended-checkbox-group/extended-checkbox-group.ts
@@ -4,7 +4,7 @@
  */
 /* eslint-disable-next-line import/no-extraneous-dependencies */
 import { Field } from '@aemforms/af-core';
-
+import { updateOrCreateInvalidMsg } from '../../util.js'
 /**
  * Decorates a custom form field component
  * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper. Refer to the documentation
@@ -17,6 +17,31 @@ export default async function decorate(fieldDiv: HTMLElement, fieldJson: Field, 
   console.log('⚙️ Decorating extended-checkbox-group component:', fieldDiv, fieldJson, parentElement, formId);
 
   fieldDiv.classList.add('extended-checkbox-group');
+  const description = fieldDiv.querySelector(':scope>.field-description');
+
+  if (description) {
+    // Move description to the bottom
+    fieldDiv.append(description);
+  }
+
+  if (fieldJson.required) {
+    const input = document.createElement('input');
+    input.required = true;
+    input.type = 'text';
+    input.style.display = 'none';
+
+    fieldDiv.dataset.requiredErrorMessage = fieldJson.mandatoryMessage || 'Please select at least one from up to four';
+
+    fieldDiv.addEventListener('change', () => {
+      const required = fieldDiv.querySelectorAll('input[type="checkbox"]:checked').length === 0;
+      if (!required) {
+        updateOrCreateInvalidMsg(input)
+      }
+      input.required = required;
+    });
+
+    fieldDiv.append(input);
+  }
 
   const toastTitleProp = fieldJson.properties.toastTitle;
   // set the 'data-toast-title' on the fieldDiv
